417ab39971270f484b49a691ad7c6056a9ed6402
Sullivan Fitz <82064336+FitzSW@users.noreply.github.com>
2022-05-03 17:43:00 -0500

    Update integrals.f90

--- integrals.f90
+++ integrals.f90
@@ -8,17 +8,29 @@ PROGRAM integrals
 ! TWO-ELECTRON is the two electron integral matrix
 ! --------------------------------------------------------------------------------------
 
-!call LAPACK functions
-EXTERNAL
+  !call LAPACK functions
+  EXTERNAL
 
-IMPLICIT NONE
+  IMPLICIT NONE
 
-REAL(N,N) :: S, H, TWO_ELECTRON
-!N here will be the size of the matrices based on basis set
-!maybe 16x16
+  REAL, DIMENSION(N,N) :: S, H, TWO_ELECTRON
+  !N here will be the size of the matrices based on basis set
+  !maybe 16x16
 
 
 
 
 
 END PROGRAM integrals
+
+SUBROUTINE Gauss_Multiply(G1,G2, PROD)
+  !May pass G1,G2 as a a vector of necessary gaussian values
+  !Should return an array of necessary gaussian values (should be same length as inputs)
+  IMPLICT NONE
+  
+  REAL, DIMENSION(N), intent(in) :: G1, G2
+  REAL, DIMENSION(N), intent(out) :: PROD
+  
+END SUBROUTINE Gauss_Multiply
+
+
