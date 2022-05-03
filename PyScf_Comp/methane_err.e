525c6c36260ab1c7af74e10bffe2b9684d33dde5
n-dohrmann <87952011+n-dohrmann@users.noreply.github.com>
2022-05-03 17:45:17 -0500

    Add files via upload

---
+++ PyScf_Comp/methane_err.e
@@ -0,0 +1,4 @@
+pyscf plugins found in 
+/home/ndohrmann/Apps/pyscf
+/home/ndohrmann/.conda/envs/local/lib/python3.7/site-packages/pyscf
+When PYTHONPATH is set, it is recommended to load these plugins through the environment variable PYSCF_EXT_PATH
---
+++ PyScf_Comp/methane_pyscf.py
@@ -0,0 +1,22 @@
+#! /home/ndohrmann/.conda/envs/local/bin/python3
+
+"""
+A comparative calculation done on the same methane geometry with the 
+same basis set (cc-pVTZ) 
+"""
+
+import pyscf
+from pyscf import gto, scf
+
+mol = gto.M(atom='C 0 0 0; H 1.032996   -0.315832    0.133490; H -0.534124   -0.097985    0.943271; H -0.476072   -0.625125   -0.753143; H -0.022800    1.038942   -0.323618', basis = 'cc-pvtz')
+            
+            
+mf = scf.hf.SCF(mol)
+mf.verbose = 0
+mf.level_shift = .4
+en = mf.scf()
+
+output_loc = "/project/lgagliardi/ndohrmann/Desktop/M25510/PySCF_Comp/pyscf_out"
+
+with open(output_loc,"w") as f:
+    f.write(str(en))
