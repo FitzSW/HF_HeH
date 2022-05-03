0728342c85ac5842ce533c4ed2a07e97492e8849
Sullivan Fitz <82064336+FitzSW@users.noreply.github.com>
2022-05-03 17:15:15 -0500

    Update integrals.f90

--- integrals.f90
+++ integrals.f90
@@ -1,9 +1,12 @@
 PROGRAM integrals
 
 
-! -------------------------------------------------------------------------
+! --------------------------------------------------------------------------------------
 ! Computes initial values of molecular integrals necessary for HF procedure
-! -------------------------------------------------------------------------
+! S is the overlap matrix
+! H = T + V, where T is the kinetic energy matrix and V is the nuclear attraction matrix
+! TWO-ELECTRON is the two electron integral matrix
+! --------------------------------------------------------------------------------------
 
 
 IMPLICIT NONE
