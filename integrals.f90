1cde5593bdb51126155daf1e818f098a3a2bb648
Sullivan Fitz <82064336+FitzSW@users.noreply.github.com>
2022-05-06 23:49:24 -0500

    Updated integral.f90 to module format

--- integrals.f90
+++ integrals.f90
@@ -1,4 +1,4 @@
-PROGRAM integrals
+MODULE integrals
 
 
 ! --------------------------------------------------------------------------------------
@@ -17,54 +17,66 @@ PROGRAM integrals
   !N here will be the size of the matrices based on basis set
   !maybe 16x16
 
-
-
-
-
-END PROGRAM integrals
-!--------------------------------
-SUBROUTINE Gauss_Multiply(G1,G2, PROD)
-  !May pass G1,G2 as a a vector of necessary gaussian values
-  !Should return an array of necessary gaussian values (should be same length as inputs)
-  !will likely be called 
-  IMPLICIT NONE
   
-  REAL, DIMENSION(N), intent(in) :: G1, G2
-  REAL, DIMENSION(N), intent(out) :: PROD
-  
-END SUBROUTINE Gauss_Multiply
-!--------------------------------
-SUBROUTINE Overlap(G1,G2,PROD)
 
-    IMPLICIT NONE
 
-    REAL, DIMENSION(N), intent(in) :: G1, G2
-    REAL, DIMENSION(N), intent(out) :: PROD
 
-END SUBROUTINE
-!--------------------------------
-SUBROUTINE Kinetic(G1, G2, PROD)
 
-    IMPLICIT NONE
 
-    REAL, DIMENSION(N), intent(in) :: G1, G2
-    REAL, DIMENSION(N), intent(out) :: PROD
+CONTAINS
+        
+        !--------------------------------
+        SUBROUTINE Gauss_Multiply(G1,G2, PROD)
+                !May pass G1,G2 as a a vector of necessary gaussian values
+                !Should return an array of necessary gaussian values (should be same length as inputs)
+                !will likely be called often
+                !Use the Gaussian Product Theorem 
+                IMPLICIT NONE
+          
+                REAL, DIMENSION(N), intent(in) :: G1, G2
+                REAL, DIMENSION(N), intent(out) :: PROD
 
-END SUBROUTINE
-!--------------------------------
-SUBROUTINE Potential(G1, G2, PROD)
+                REAL :: gama, p, 
 
-    IMPLICIT NONE
+          
+          
+        END SUBROUTINE Gauss_Multiply
+        !--------------------------------
+        SUBROUTINE Overlap(G1,G2,PROD)
 
-    REAL, DIMENSION(N), intent(in) :: G1, G2
-    REAL, DIMENSION(N), intent(out) :: PROD
+            IMPLICIT NONE
 
-END SUBROUTINE
-!--------------------------------
-SUBROUTINE TWO_ELECTRON
-    IMPLICIT NONE
+            REAL, DIMENSION(N), intent(in) :: G1, G2
+            REAL, DIMENSION(N), intent(out) :: PROD
 
-    REAL, DIMENSION(N), intent(in) :: G1, G2
-    REAL, DIMENSION(N), intent(out) :: PROD
+        END SUBROUTINE
+        !--------------------------------
+        SUBROUTINE Kinetic(G1, G2, PROD)
 
-END SUBROUTINE
+            IMPLICIT NONE
+
+            REAL, DIMENSION(N), intent(in) :: G1, G2
+            REAL, DIMENSION(N), intent(out) :: PROD
+
+        END SUBROUTINE
+        !--------------------------------
+        SUBROUTINE Potential(G1, G2, PROD)
+
+            IMPLICIT NONE
+
+            REAL, DIMENSION(N), intent(in) :: G1, G2
+            REAL, DIMENSION(N), intent(out) :: PROD
+
+        END SUBROUTINE
+        !--------------------------------
+        SUBROUTINE TWO_ELECTRON
+            IMPLICIT NONE
+
+            REAL, DIMENSION(N), intent(in) :: G1, G2
+            REAL, DIMENSION(N), intent(out) :: PROD
+
+        END SUBROUTINE
+
+
+
+END MODULE integrals
