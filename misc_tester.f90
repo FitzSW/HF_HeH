754a63fe1c144c1cd4b86a9f551fac8187467150
Noah Dohrmann <noah@dohrmann.org>
2022-05-10 20:11:26 -0500

    simplified orbitals (no p orbitals)

--- misc_tester.f90
+++ misc_tester.f90
@@ -4,27 +4,25 @@ program tester
 
     implicit none
 
+    character(len=1) :: orb_type
+    character(len=1) :: atom_name
+    real, dimension(3) :: coords
+    real, dimension(3) :: coeff_vec
+    real, dimension(3) :: expo_vec
+    integer :: M
     type(contracted_gto) :: A
 
-    A%coords = [0.0,0.0,0.0]
+    M = 3
 
-    A = new_contraction(3)
+    orb_type = "s"
+    atom_name = "H"
+    coords = [0.0,0.0,0.0]
+    ! The first s on 6-31G Hydrogen
+    coeff_vec = [0.1873113696D+02, 0.2825394365D+01, 0.6401216923D+00]
+    expo_vec = [0.3349460434D-01, 0.2347269535D+00, 0.8137573261D+00]
 
-    A%orb_type = "p"
-    A%atom_name = "C"
 
-    ! The angular momentum numbers
-    A%contraction(1)%L = [1,0,0]
-    A%contraction(2)%L = [0,1,0]
-    A%contraction(3)%L = [0,0,1]
-
-    ! coeff and exponents
-    A%contraction(1)%coeff =  4.912920 
-    A%contraction(1)%expo = 0.109931
-    A%contraction(2)%coeff =  4.912920 
-    A%contraction(2)%expo = 0.109931
-    A%contraction(3)%coeff =  4.912920 
-    A%contraction(3)%expo = 0.109931
+    A = new_contraction(M,orb_type,atom_name,coords,coeff_vec,expo_vec)
 
     ! write out a test
     write (*,*) "Atom name for A: :", A%atom_name
--- orbitals.f90
+++ orbitals.f90
@@ -4,9 +4,9 @@ module orbitals
 implicit none
 
 type primitive_gto
-    ! momentum numbers will vary across a p orb contraction
-    integer, dimension(3) :: L   ! angular momentum
     ! the coefficients and expoents will vary
+    ! Only for s - type orbitals, does not mention 
+    ! angular momentum
     real :: coeff
     real :: expo
 end type primitive_gto
@@ -15,19 +15,42 @@ type contracted_gto
     character(len=1) :: orb_type
     character(len=1) :: atom_name
     real, dimension(3) :: coords ! coordinates
-    ! length of the contraction (P orbitals will have 3N as s orbitals)
+    ! length of the contraction 
     integer :: M 
+    ! The total number of primitives in the contraction
     type(primitive_gto), allocatable, dimension(:) :: contraction
 end type contracted_gto
 
 contains
 !
-function new_contraction(M)!,orb_type,atom_name,coords,coeff_vec,expo_vec)
+function new_contraction(M,orb_type,atom_name,coords,coeff_vec,expo_vec)
     implicit none
     integer, intent(in) :: M
+    character(len=1), intent(in) :: orb_type ! should all be s
+    character(len=1), intent(in) :: atom_name
+    real, dimension(3), intent(in) :: coords
+    ! do these need to be allocatable?
+    real, dimension(M),intent(in) :: coeff_vec
+    real, dimension(M),intent(in) :: expo_vec
+
+    integer :: i
+
     type(contracted_gto) :: new_contraction
+
+
     allocate(new_contraction%contraction(M))
     new_contraction%M = M
+    new_contraction%orb_type = orb_type
+    new_contraction%atom_name = atom_name
+    new_contraction%coords = coords
+
+    ! call coefficient_expo_maker(new_contraction,coeff_vec,expo_vec)
+
+    do i = 1, M
+        new_contraction%contraction(i)%coeff = coeff_vec(i)
+        new_contraction%contraction(i)%expo = expo_vec(i)
+    enddo
+
 end function new_contraction
 
 
