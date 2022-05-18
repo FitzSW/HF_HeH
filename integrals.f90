894e8aceee28172caf418a46cd1fdfa5ab7b6862
Sullivan Fitz <82064336+FitzSW@users.noreply.github.com>
2022-05-18 03:16:32 -0500

    Added some structure to potential and two electron integrals

--- integrals.f90
+++ integrals.f90
@@ -67,20 +67,68 @@ CONTAINS
 
         END SUBROUTINE
         !--------------------------------
-        SUBROUTINE Potential(alpha_a,alpha_b, R_a, R_b, P)
+        SUBROUTINE Potential(alpha_a,alpha_b, R_a, R_b, R_c, Z_c, V)
+        !Between two GTOs and the nucleus of either H or He
+        !R_c is the center of this nucleus, Z_c is the nuclear charge 
+        !The final nuclear attraction matrix will be evaluated over all GTO pairs for the two nuclei
 
             IMPLICIT NONE
 
-            REAL, intent(in) :: alpha_a, alpha_b, R_a, R_b
-            REAL, intent(out) :: P
+            REAL, intent(in) :: alpha_a, alpha_b, Z_c
+            REAL, dimension(3), intent(in) :: R_a, R_b, R_c
+            REAL, intent(out) :: V
+            
+            REAL :: konstant, p
+            REAL, dimension(3) :: R_p, vec_new
+            
+            CALL Gauss_Multiply(alpha_a, alpha_b, R_a, R_b, konstant, R_p, p)
+            
+            !Szabo and Ostlund don't mention these two cases, but another report I found does, so I will include them just in case
+            !To be clear, Szabo and Ostlund just includes the R_p != R_c case
+            
+            IF (R_p.eq.R_c) THEN
+            
+            V = 
+            
+            ELSE IF (R_p.ne.R_c) THEN
+            
+            V = 
+            
+            
+            
+            
 
         END SUBROUTINE
         !--------------------------------
-        SUBROUTINE TWO_ELECTRON(alpha_a,alpha_b, R_a, R_b, TE)
+        SUBROUTINE TWO_ELECTRON(alpha_a,alpha_b, alpha_c, alpha_d, R_a, R_b, R_c, R_d, TE)
             IMPLICIT NONE
 
-            REAL, intent(in) :: alpha_a,alpha_b, R_a, R_b
+            REAL, intent(in) :: alpha_a,alpha_b, alpha_c, alpha_d
+            REAL, dimension(3), intent(in) :: R_a, R_b, R_c, R_d
             REAL, intent(out) :: TE
+            
+            REAL :: konstant_p, konstant_q, p, q
+            REAL, dimension(3) :: R_p, R_q, vec_new1, vec_new2
+            !R_p is combined center of Gauss Product a,b; R_q is combined center of gauss product, c,d
+            
+            !multiply a,b
+            CALL Gauss_Multiply(alpha_a, alpha_b, R_a, R_b, konstant_p, R_p, p)
+            !multiply c,d
+            CALL Gauus_Multiply(alpha_c, alpha_d, R_c, R_d, konstant_q, R_q, q)
+            
+            !There are two possible results for this integral based on if R_p and R_q are equal
+            
+            IF (R_p.eq.R_q) THEN
+            
+              !case one
+            
+            ELSE IF (R_p.ne.R_q) THEN
+            
+              !case two
+            
+            
+         
+            
 
         END SUBROUTINE
 
