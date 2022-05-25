2953ce69293703a0263956c6525972af1f164fd9
Sullivan Fitz <82064336+FitzSW@users.noreply.github.com>
2022-05-25 14:25:02 -0500

    Added G Matrix

--- Matrices.f90
+++ Matrices.f90
@@ -15,7 +15,7 @@ CONTAINS
         SUBROUTINE Compute_Overlap(OM)
 
 
-                REAL, dimension(6,6), intent(inout) :: OM
+                REAL, dimension(6,6), intent(out) :: OM
 
                 INTEGER :: mu,nu,i,j,i_f,j_f
 
@@ -29,6 +29,8 @@ CONTAINS
                         ELSE
                                 i_f = 1
 
+                        END IF
+
                         DO nu = 1,6
                                 
                                 !Specificy length of contraction for nu
@@ -37,7 +39,7 @@ CONTAINS
                                 ELSE
                                         j_f = 1
                                         
-                                
+                                END IF
                                 !Iterate over the length of both contractions in order to compute matrix element
                                 DO i = 1, i_f
 
@@ -61,7 +63,7 @@ CONTAINS
         SUBROUTINE Compute_Kinetic(KM)
 
         
-                REAL, dimension(6,6), intent(inout) :: KM
+                REAL, dimension(6,6), intent(out) :: KM
 
                 INTEGER :: mu, nu, i, j, i_f, j_f
 
@@ -75,6 +77,8 @@ CONTAINS
                         ELSE
                                 i_f = 1
 
+                        END IF
+
                         DO nu = 1,6
                                 
                                 !Specificy length of contraction for nu
@@ -83,7 +87,7 @@ CONTAINS
                                 ELSE
                                         j_f = 1
                                         
-                                
+                                END IF
                                 !Iterate over the length of both contractions in order to compute matrix element
                                 DO i = 1, i_f
 
@@ -107,7 +111,7 @@ CONTAINS
         SUBROUTINE  Compute_Potential(PM)
 
                 
-                REAL, dimension(6,6), intent(inout) :: PM
+                REAL, dimension(6,6), intent(out) :: PM
                 INTEGER :: mu,nu,i,j,i_f,j_f, ai
                 CHARACTER(len=2) :: atom
                 CHARACTER(len=2), dimension(2) :: atom_list
@@ -130,6 +134,8 @@ CONTAINS
                                 ELSE
                                         i_f = 1
 
+                                END IF
+
                                 DO nu = 1,6
                                         
                                         !Specificy length of contraction for nu
@@ -138,7 +144,7 @@ CONTAINS
                                         ELSE
                                                 j_f = 1
                                                 
-                                        
+                                        END IF
                                         !Iterate over the length of both contractions in order to compute matrix element
                                         DO i = 1, i_f
 
@@ -156,6 +162,84 @@ CONTAINS
 
         END SUBROUTINE
 
+
+
+        SUBROUTINE Compute_G(P, GM):
+
+                IMPLICIT NONE
+
+                REAL, dimension(6,6), intent(in) :: P !Density matrix
+
+                REAL, dimension(6,6), intent(out) :: GM
+
+                INTEGER :: mu,nu,lambda,sigma,i,j,k,l,i_f,j_f,k_f,l_f
+
+                
+                DO mu = 1,6
+                        
+                        !specificy length of contraction for mu
+                        IF ((mu.eq.1).or.(mu.eq.4) THEN
+                                i_f = 3
+                        ELSE
+                                i_f=1
+                        END IF
+
+                        DO nu = 1,6
+                                
+                                !specificy length of contraction
+                                IF ((nu.eq.1).or.(nu.eq.4) THEN
+                                        j_f = 3
+                                ELSE
+                                        j_f = 1
+                                END IF
+
+                                DO lambda = 1,6
+
+                                        !specify length of contraction
+                                        IF ((lambda.eq.1).or.(lambda.eq.4) THEN
+                                                k_f = 3
+                                        ELSE
+                                                k_f = 1
+                                        END IF
+
+                                        DO sigma = 1,6
+                                                
+                                                !specificy length of contraction
+                                                IF ((sigma.eq.1).or.(sigma.eq.4) THEN
+                                                        l_f = 3
+                                                ELSE
+                                                        l_f = 1
+                                                END IF
+
+                                                
+                                                !iterate over primitives
+
+                                                DO i = 1,i_f
+                                                        DO j = 1,j_f
+                                                                DO k = 1, k_f
+                                                                        DO l = 1, l_f
+                                                                                
+                                                                                GM(mu,nu) = GM(mu,nu) +
+                                                                                P(lambda,sigma)*(Two_Electron_Prim(mu,nu,lambda,sigma,i,j,k,l)
+                                                                                - (1/2) * Two_Electron_Prim(mu,lambda,sigma,nu,i,k,l,j))
+                                                                                
+
+                                                                        END DO
+
+                                                                END DO
+                                                        END DO
+                                                END DO
+                                        END DO
+                                END DO
+                        END DO
+                END DO
+                                        
+
+
+                        
+
+
+END MODULE
 
                 
 
