ff97716839f18a2975555f46d0751c57a1bc37d8
Noah Dohrmann <noah@dohrmann.org>
2022-05-26 16:16:41 -0500

    instructions on pdf clarified

---
+++ mac_build.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+
+gfortran constants.f90
+gfortran diagonalizer.f90
+# gfortran integrals.f90
+gfortran orbitals.f90
+gfortran reader.f90
+# gfortran Matrices.f90
+# gfortran Gauss_Multiply.f90
+gfortran misc_tester.f90  diagonalizer.f90  orbitals.f90 reader.f90 constants.f90 #Matrices.f90 Gauss_Multiply.f90 constants.f90 integrals.f90
+echo "
+
+Fortran base files built!...
+
+Compiling matrix executables...
+"
+
+c++ -std=c++20  -O3 -Wall -Wextra Matrix_IO/cc_x_finder.cc -o ./cc_x_finder
+c++ -std=c++20  -O3 -Wall -Wextra Matrix_IO/cc_eigen_finder.cc -o ./cc_eigen_finder
+
+echo "
+
+Project successfully built! ...
+
+"
+
+echo "Running HF SCF Procedure..."
+
+./a.out
+
+
+
+echo "Done! Output written to 'hf_out.out'"
+rm a.out
+rm cc_eigen_finder
+rm cc_x_finder
