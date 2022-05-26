edfa9c8fda4d49340dbbb2d6285f4ad70bf857c9
Noah Dohrmann <noah@dohrmann.org>
2022-05-26 15:50:47 -0500

    fixed mac vs linux sed issue

---
+++ linux_eigen.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# This is a script to adapt the Eigen header files include statements for
+# local use if you are doing a local installation of the "Eigen" directory
+# from the linked gitlab
+
+# THIS SHOULD NOT BE RUN IF YOU ALREADY HAD ACCESS TO EIGEN
+
+# Clone the full eigen directory
+git submodule add https://gitlab.com/libeigen/eigen
+# Select for the necessary files and delete the rest
+mv eigen/Eigen Matrix_IO
+rm -rf eigen
+
+sed -i -e 's/<Eigen\/Dense>/"Eigen\/Dense"/g' Matrix_IO/cc_x_finder.cc
+sed -i -e 's/<Eigen\/Dense>/"Eigen\/Dense"/g' Matrix_IO/cc_eigen_finder.cc
+sed -i -e 's/<Eigen\/Eigenvalues>/"Eigen\/Eigenvalues"/g' Matrix_IO/cc_x_finder.cc
+sed -i -e 's/<Eigen\/Eigenvalues>/"Eigen\/Eigenvalues"/g' Matrix_IO/cc_eigen_finder.cc
