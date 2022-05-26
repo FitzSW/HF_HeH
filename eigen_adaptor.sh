a51a869b28a0c913d636e3397c7528d33c15d073
Noah Dohrmann <noah@dohrmann.org>
2022-05-26 15:42:20 -0500

    updated the eigen adaptor script

--- eigen_adaptor.sh
+++ eigen_adaptor.sh
@@ -6,6 +6,12 @@
 
 # THIS SHOULD NOT BE RUN IF YOU ALREADY HAD ACCESS TO EIGEN
 
+# Clone the full eigen directory
+git submodule add https://gitlab.com/libeigen/eigen
+# Select for the necessary files and delete the rest
+mv eigen/Eigen Matrix_IO
+rm -rf eigen
+
 sed -i '' -e 's/<Eigen\/Dense>/"Eigen\/Dense"/g' Matrix_IO/cc_x_finder.cc
 sed -i '' -e 's/<Eigen\/Dense>/"Eigen\/Dense"/g' Matrix_IO/cc_eigen_finder.cc
 sed -i '' -e 's/<Eigen\/Eigenvalues>/"Eigen\/Eigenvalues"/g' Matrix_IO/cc_x_finder.cc
